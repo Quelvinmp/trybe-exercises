16e3fc84e3d9492a944ec941e49f30323613f7cc
Quelvinmp <quelvinmp@gmail.com>
2022-11-23 13:41:45 -0300

    exercícios de 1 a 4 concluídos

---
+++ 01-fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/scripts.js
@@ -0,0 +1,63 @@
+    // Exercício 1
+
+// const a = 10;
+// const b = 50;
+
+// let adicao = a + b;
+// let subtracao = a- b;
+// let multiplicacao = a * b;
+// let divisao = a / b;
+// let modulo = a % b;
+
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(modulo);
+
+    // Exercício 2
+
+// const a = 30;
+// const b = 50;
+// let resultado;
+
+// if (a < b) {
+//     console.log(resultado = 'a é menor que b');
+// } else if (a > b) {
+//     console.log(resultado = 'a é maior que b');
+// } else if (a == b) {
+//     console.log(resultado = 'a é igual a b');
+// }
+
+    // Exercício 3
+
+// const a = 30;
+// const b = 50;
+// const c = 70;
+// let resultado;
+
+// if (a > b && a > c) {
+//     console.log(resultado = 'a é o maior');
+// } else if (b > a && b > c) {
+//     console.log(resultado = 'b é o maior');
+// } else if (c > a && c > b) {
+//     console.log(resultado = 'c é o maior');
+// } else {
+//     console.log(resultado = 'Não tem maior');
+// }
+
+    // Exercício 4
+
+// const num = 0;
+
+// if (num > 0) {
+//     console.log('Positive');
+// } else if (num < 0) {
+//     console.log('Negative');
+// } else {
+//     console.log('Zero');
+// }
+
+    // Exercício 5
+
+
