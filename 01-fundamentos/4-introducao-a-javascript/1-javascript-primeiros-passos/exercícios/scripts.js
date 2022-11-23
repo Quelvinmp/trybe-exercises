f7cb7c85624d9e875f3d5ff16bcf0d598a1e2060
Quelvinmp <quelvinmp@gmail.com>
2022-11-23 15:25:53 -0300

    Exercício 8 Concluído

--- 01-fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/scripts.js
@@ -107,3 +107,23 @@
 
     // Exercício 7
 
+// const nota = 100;
+
+// if (nota >= 90 && nota <= 100) {
+//     console.log('A');
+// } else if (nota >= 80 && nota < 100) {
+//     console.log('B');
+// } else if (nota >= 70 && nota < 100) {
+//     console.log('C');
+// } else if (nota >= 60 && nota < 100) {
+//     console.log('D');
+// } else if (nota >= 50 && nota < 100) {
+//     console.log('E');
+// } else if (nota < 50 && nota >= 0) {
+//     console.log('F');
+// } else {
+//     console.log('error');
+// }
+
+    // Exercício 8
+
