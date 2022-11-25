062ef1ad3a0a3a41cc3fd1eec0d9a9827f30e648
Quelvinmp <quelvinmp@gmail.com>
2022-11-25 10:43:19 -0300

    Requisitos de 1 a 7 Concluídos

---
+++ 01-fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercício/scripts.js
@@ -0,0 +1,84 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+    // Requisito 1
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);    
+// }
+
+
+    // Requisito 2
+
+// let sum = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum += numbers[index];
+// }
+
+// console.log(sum);
+
+
+    // Requisito 3 e 4
+
+// let sum = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum += numbers[index];
+// }
+
+// let media = sum / numbers.length;
+
+// // console.log(media);
+
+// if (media > 20) {
+//     console.log('Valor maior que 20');
+// } else if (media < 20) {
+//     console.log('Valor menor que 20');
+// }
+
+
+    // Requisito 5
+
+// let higher = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > higher) {
+//         higher = numbers[index]
+//     }
+// }
+
+// console.log(higher);
+
+
+    // Requisito 6
+
+// let odd = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 !== 0) {
+//         odd += 1;
+//     }
+// }
+
+// if (odd === 0) {
+//     console.log('nenhum valor ímpar encontrado');
+// } else {
+//     console.log(odd);
+// }
+
+
+    // Requisito 7
+
+// let lower = Infinity;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] < lower ) {
+//         lower = numbers[index]
+//     }
+// }
+
+// console.log(lower);
+
+
+    // Requisito 8
+
