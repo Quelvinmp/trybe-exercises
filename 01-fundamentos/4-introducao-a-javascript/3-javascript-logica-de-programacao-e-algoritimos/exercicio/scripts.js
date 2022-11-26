41452c6ebd843dc8221fd2330a07ab06797f1689
Quelvinmp <quelvinmp@gmail.com>
2022-11-26 11:56:18 -0300

    Requisitos 1 e 2 Concluídos

---
+++ 01-fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicio/scripts.js
@@ -0,0 +1,23 @@
+    // Requisito 1
+
+// const number = 10;
+// let fatorial = number;
+
+// for (let index = number -1; index >= 1; index -= 1) {
+//     fatorial *= index;
+// }
+
+// console.log(fatorial);
+
+
+    // Requisito 2
+
+// let word = 'pneumoultramicroscopicoscilicovulcanicomiose';
+// let inverseWord = '';
+
+// for (let index = word.length-1; index >= 0; index -= 1) {
+//     inverseWord += word[index];
+// }
+// console.log(inverseWord);
+
+    // Requisito 3
