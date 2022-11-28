0ec02000a9a44533375cabaca4e7dcbecadc92f2
Quelvinmp <quelvinmp@gmail.com>
2022-11-28 18:53:42 -0300

    Requisito 4 Concluído

--- 01-fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicio-bonus/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicio-bonus/scripts.js
@@ -28,52 +28,40 @@
 
 // Requisito 3
 
-let geometricForm = '';
-let limit = 5;
-
-for (let i = 1; i <= limit; i += 1) {
-    while (geometricForm.length +1 <= limit - i) {
-        geometricForm += ' ';
-    }
-    for (let i2 = 1; i2 <= i; i2 += 1) {
-        geometricForm += '*';
-    }
-    console.log(geometricForm);
-    geometricForm = '';
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Requisito 4
-
 // let geometricForm = '';
 // let limit = 5;
 
 // for (let i = 1; i <= limit; i += 1) {
-//     while (geometricForm.length <= (limit / 2) - i) {
+//     while (geometricForm.length +1 <= limit - i) {
 //         geometricForm += ' ';
 //     }
 //     for (let i2 = 1; i2 <= i; i2 += 1) {
 //         geometricForm += '*';
 //     }
-//     while (geometricForm.length <= limit) {
-//         geometricForm += ' ';
-//     }
 //     console.log(geometricForm);
 //     geometricForm = '';
 // }
+
+
+// Requisito 4
+
+const limit = 15;
+let geometricForm = '';
+let leftSpace = '';
+let rightSpace = '';
+let asterisk = '';
+
+for (let i = 1; i <= limit; i += 2) {
+    while (geometricForm.length + 1 <= ((limit - i) / 2).toFixed()) {
+        geometricForm += ' ';
+    }
+    for (let j = 1; j <= i; j += 1) {
+        geometricForm += '*';
+    }
+    while (geometricForm.length + 1 <= limit) {
+        geometricForm += ' ';
+    }
+    console.log(geometricForm);
+    geometricForm = '';
+}
+
