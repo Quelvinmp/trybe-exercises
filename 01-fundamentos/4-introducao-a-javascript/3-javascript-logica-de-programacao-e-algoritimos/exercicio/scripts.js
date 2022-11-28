9ccccd723a4f68180cbfaf3f1346f3010d2bd4f4
Quelvinmp <quelvinmp@gmail.com>
2022-11-28 16:04:32 -0300

    Requisito 4 Concluído

--- 01-fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicio/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicio/scripts.js
@@ -46,3 +46,13 @@
 
     // Requisito 4
 
+// let primeNumber;
+
+// for (let i = 2; i <= 50; i += 1) {
+//     if (i % 2 !== 0 && i % 3 !==0 && i % 5 !== 0 && i % 7 !== 0) {
+//         primeNumber = i;
+//     } else if (i === 2 || i === 3 || i === 5 || i === 7) {
+//         primeNumber = i;
+//     }
+// } 
+// console.log(primeNumber);
