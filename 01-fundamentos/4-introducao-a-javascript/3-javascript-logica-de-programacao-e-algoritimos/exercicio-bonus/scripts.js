7fe38d2666bd724788a2909eee78dd9892b106b3
Quelvinmp <quelvinmp@gmail.com>
2022-11-28 21:09:17 -0300

    Requisito 5 Concluído

--- 01-fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicio-bonus/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercicio-bonus/scripts.js
@@ -45,23 +45,66 @@
 
 // Requisito 4
 
-const limit = 15;
-let geometricForm = '';
-let leftSpace = '';
-let rightSpace = '';
-let asterisk = '';
+// const limit = 5;
+// let geometricForm = '';
 
-for (let i = 1; i <= limit; i += 2) {
-    while (geometricForm.length + 1 <= ((limit - i) / 2).toFixed()) {
-        geometricForm += ' ';
-    }
-    for (let j = 1; j <= i; j += 1) {
-        geometricForm += '*';
-    }
-    while (geometricForm.length + 1 <= limit) {
-        geometricForm += ' ';
-    }
-    console.log(geometricForm);
-    geometricForm = '';
-}
+// for (let i = 1; i <= limit; i += 2) {
+//     while (geometricForm.length + 1 <= ((limit - i) / 2).toFixed()) {
+//         geometricForm += ' ';
+//     }
+//     for (let j = 1; j <= i; j += 1) {
+//         geometricForm += '*';
+//     }
+//     while (geometricForm.length + 1 <= limit) {
+//         geometricForm += ' ';
+//     }
+//     console.log(geometricForm);
+//     geometricForm = '';
+// }
 
+// Gabarito
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// let midOfMatrix = (n + 1) / 2;
+// let controlLeft = midOfMatrix;
+// let controlRight = midOfMatrix;
+
+// for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+//   for (let columnIndex = 1; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex > controlLeft && columnIndex < controlRight) {
+//       inputLine = inputLine + symbol;
+//     } else {
+//       inputLine = inputLine + ' ';
+//     }
+//   }
+//   console.log(inputLine);
+//   inputLine = '';
+//   controlRight += 1;
+//   controlLeft -= 1
+// };
+
+
+// Requisito 5 - Gabarito
+
+// let n = 7;
+// let middle = (n + 1) / 2;
+// let controlLeft = middle;
+// let controlRight = middle;
+// let symbol = '*';
+
+// for (let line = 1; line <= middle; line += 1) {
+//   let outputLine = '';
+//   for (let col = 1; col <= n; col += 1) {
+//     if (col == controlLeft || col == controlRight || line == middle) {
+//       outputLine += symbol;
+//     } else {
+//       outputLine += ' ';
+//     }
+//   }
+//   controlLeft -= 1;
+//   controlRight += 1;
+//   console.log(outputLine);
+// }
