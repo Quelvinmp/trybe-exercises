00b6b803871355ce87fe1ae791b8ba26e04c1773
Quelvinmp <quelvinmp@gmail.com>
2022-11-29 10:53:39 -0300

    Para fixar: Objetos - feito

--- 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/para fixar/objetos/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/para fixar/objetos/scripts.js
@@ -1,7 +1,7 @@
-let name = 'Marta';
-let lastName = 'Silva';
-let age = 34;
-let medals = { golden: 2, silver: 3 };
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
 
 let player = {
     name: 'Marta',
@@ -14,4 +14,12 @@ let player = {
 }
 
 
-console.table(player);
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+
+player.bestInTheWorld = [
+    2006, 2007, 2008, 2009, 2010, 2018
+]
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes');
+
+console.log('A jogadora possui ' + player.medals['golden'] + ' medalhas de ouro e ' + player.medals['silver'] + ' medalhas de prata');
