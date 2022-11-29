c933aea08bec502e3cdd1fcb8720c92eb04b9f68
Quelvinmp <quelvinmp@gmail.com>
2022-11-29 11:42:32 -0300

    para fixar - funcoes - concluido

---
+++ 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/para fixar/funcoes/scripts.js
@@ -0,0 +1,92 @@
+// Requisito 1
+
+// function sum (a, b) {
+//     return a + b;
+// }
+
+// function sub (a, b) {
+//     return a - b;
+// }
+
+// function mult (a, b) {
+//     return a * b;
+// }
+
+// function div (a, b) {
+//     return a / b;
+// }
+
+// function mod (a, b) {
+//     return a % b;
+// }
+
+// console.log(sum(3,5));
+// console.log(sub(15,10));
+// console.log(mult(3,5));
+// console.log(div(15,5));
+// console.log(mod(5,3));
+
+
+// Requisito 2
+
+// const number1 = 50;
+// const number2 = 65;
+
+// function higherNumber (n1, n2) {
+//     if (n1 > n2) {
+//         console.log(n1 + ' É o maior!');
+//     } else {
+//         console.log(n2 + ' É o maior!' );
+//     }
+// }
+
+// higherNumber(number1, number2);
+
+
+// Requisito 3
+
+// const number1 = 50;
+// const number2 = 65;
+// const number3 = 100;
+
+// function higherNumber (n1, n2, n3) {
+//     if (n1 > n2 && n1 > n3) {
+//         console.log(n1 + ' É o maior!');
+//     } else if (n2 > n1 && n2 > n3) {
+//         console.log(n2 + ' É o maior!');
+//     } else {
+//         console.log(n3 + ' É o maior!');
+//     }
+// }
+
+// higherNumber(number1, number2, number3)
+
+
+// Requisito 4
+
+// function naturalOrNo (number) {
+//     if (number > 0) {
+//         console.log('Positive!');
+//     } else if (number < 0) {
+//         console.log('Negative!');
+//     } else {
+//         console.log('Zero!');
+//     }
+// }
+
+// naturalOrNo(0);
+
+
+// Requisito 5
+
+// function triangleAngle (ang1, ang2, ang3) {
+//     if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
+//         console.log('Erro!');
+//     } else if  (ang1 + ang2 + ang3 === 180) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// triangleAngle(60, 60, 60)
