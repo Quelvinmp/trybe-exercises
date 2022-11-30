cb3dfa7a2fb96b3e97a834bc6a083e07c56e3244
Quelvinmp <quelvinmp@gmail.com>
2022-11-30 11:16:17 -0300

    Requisito 1 Concluído

---
+++ 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/exercicios/funçoes/scripts.js
@@ -0,0 +1,26 @@
+// let word = 'arara';
+
+// function verifyPalindrome (word) {
+
+// let split = word.split('');
+// let reverse = split.reverse();
+// let join = reverse.join('');
+// let reversed = join;
+// let palindrome = false;
+
+// for (let index = 0; index < word.length; index += 1) {
+//     if (word[index] === word[word.length -1 - index]) {
+//         palindrome = true;
+//     } else {
+//         palindrome = false;
+//     }
+// }
+
+
+
+//     return palindrome
+// }
+
+// console.log(verifyPalindrome(word));         // Requisito 1
+
+
