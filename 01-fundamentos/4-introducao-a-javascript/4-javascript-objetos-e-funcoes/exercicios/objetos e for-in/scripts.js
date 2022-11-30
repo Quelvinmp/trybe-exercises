21da25cc14d8c034935312c4738711782067c7ff
Quelvinmp <quelvinmp@gmail.com>
2022-11-30 10:22:05 -0300

    arruma espaçamento de codigo

--- 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/exercicios/objetos e for-in/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/exercicios/objetos e for-in/scripts.js
@@ -1,9 +1,9 @@
 // EXERCÍCIO 1
 
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
 // console.log('Bem-vinda, ' + info.personagem);            // Requisito 1
@@ -21,10 +21,10 @@ info['recorrente'] = 'Sim';
 // }            // Requisito 4
 
 let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: 'Sim',
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
 }
 
 // for (let key in info, info2) {
@@ -40,17 +40,17 @@ let info2 = {
 // EXERCÍCIO 2 - LEITURA DE OBJETOS
 
 let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
 
 // console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama \'' + leitor.livrosFavoritos[0]['titulo'] + '\''); // Requisito 1
 
@@ -62,4 +62,4 @@ leitor.livrosFavoritos.push(
   },
 ); // Requisito 2
 
-console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos'); // Requisito 3
+// console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos'); // Requisito 3
