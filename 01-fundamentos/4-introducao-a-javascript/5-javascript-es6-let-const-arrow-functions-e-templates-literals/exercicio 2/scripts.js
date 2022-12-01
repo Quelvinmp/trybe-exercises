cb9e96ad9d5c530a3b44aed953208b4d5f8ee7c3
Quelvinmp <quelvinmp@gmail.com>
2022-12-01 11:36:31 -0300

    Resolve requisitos do segundo exercicio

---
+++ 01-fundamentos/4-introducao-a-javascript/5-javascript-es6-let-const-arrow-functions-e-templates-literals/exercicio 2/scripts.js
@@ -0,0 +1,28 @@
+// let status = 'desligado';
+
+// const onOff = () => (
+//     status.toLowerCase() === 'ligado' ? `O motor está ligado` : `O motor está desligado`
+// );
+
+// console.log(onOff());           // Requisito 1
+
+// const pi = Math.PI;
+// let raio = 5;
+// const circleArea = (raio) => (`Essa é a área do círculo: ${pi * (raio**2)}`);
+
+// console.log(circleArea(raio));          // Requisito 2
+
+
+// let word = 'Antônio foi no banheiro e não sabemos o que aconteceu';
+// let arrayWord = word.split(' ',);
+// let biggestWord = arrayWord[0];
+
+// // for (let index = 0; index < arrayWord.length; index += 1) {
+// //     if (arrayWord[index].length > biggestWord.length) {
+// //         biggestWord = arrayWord[index];
+// //     }
+// // }
+
+// const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+// console.log(longestWord("Antonio foi ao banheiro e não sabemos o que aconteceu"));           // Requisito 3
+
