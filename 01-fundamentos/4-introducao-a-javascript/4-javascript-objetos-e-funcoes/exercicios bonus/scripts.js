7b795aa0d5f90f46541c08454e7ad71c63e143b3
Quelvinmp <quelvinmp@gmail.com>
2022-12-01 18:57:50 -0300

    exercicio bonus 3 concluído

--- 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/exercicios bonus/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/exercicios bonus/scripts.js
@@ -1,6 +1,6 @@
-// EXERCÍCIO BÔNUS 1
+// // EXERCÍCIO BÔNUS 1
 
-// let romanNumeral = 'MCMXLIX'; 
+// let romanNumeral = 'MCMXLIX';
 
 // let equivalenceTable = {
 //     I: 1,
@@ -25,22 +25,22 @@
 //                 finalAlgarism = equivalenceTable[romanArray[index]];
 //             }
 //         }
-//         translate.push(finalAlgarism)
+//         naturalArray.push(finalAlgarism)
 //     }
 
-//     for (let index = 0; index < translate.length; index += 1) {
-//         if (translate[index + 1] > translate[index]) {
-//             sumTranslate -= translate[index];
+//     for (let index = 0; index < naturalArray.length; index += 1) {
+//         if (naturalArray[index + 1] > naturalArray[index]) {
+//             naturalNumber -= naturalArray[index];
 //         } else {
-//             sumTranslate += translate[index];
+//             naturalNumber += naturalArray[index];
 //         }
 //     }
 
 
-//     return sumTranslate
+//     return naturalNumber
 // }
 
-// console.log(traductionNumber(romanNumeral));                 // EXERCÍCIO BÔNUS 1
+// console.log(convertRomanToNatural(romanNumeral));                 // EXERCÍCIO BÔNUS 1
 
 
 // EXERCÍCIO BÔNUS 2
@@ -62,4 +62,66 @@
 //     return evenNumber
 // }
 
-// console.log(arrayOfNumbers(vector));                    // EXERCÍCIO BÔNUS 2                     
+// console.log(arrayOfNumbers(vector));                    // EXERCÍCIO BÔNUS 2
+
+
+// EXERCÍCIO BÔNUS 3
+
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+const separeteSingleWords = wordsArray => {
+
+    wordsArray.sort();
+
+    const uniqueWords = [wordsArray[0]];
+    for (let index = 1; index < wordsArray.length; index += 1) {
+        if (wordsArray[index] !== wordsArray[index + 1] && index + 1 !== wordsArray.length) {
+            uniqueWords.push(wordsArray[index + 1])
+        }
+    }
+    return uniqueWords
+}
+
+const repetitionsInObject = (singleWords, wordsArray) => {
+
+    const singleWordsRepetitions = {};
+    const numberOfRepetitions = [];
+
+    for (let value of singleWords) {
+        let count = 0;
+        for (let index in wordsArray) {
+            if (value === wordsArray[index]) {
+                count += 1
+            }
+        }
+        numberOfRepetitions.push(count);
+        for (let indexNumber in numberOfRepetitions) {
+            singleWordsRepetitions[value] = numberOfRepetitions[indexNumber];
+        }
+    }
+
+    return singleWordsRepetitions
+}
+
+console.log(repetitionsInObject(separeteSingleWords(basket), basket));                  // EXERCÍCIO BÔNUS 3
+
+
+// EXERCÍCIO BÔNUS 4
