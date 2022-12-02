a7ffd6ea2fed880e05928582093df7f39422fd0c
Quelvinmp <quelvinmp@gmail.com>
2022-12-02 11:54:41 -0300

    resolve exercicio bonus 4

--- 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/exercicios bonus/scripts.js
+++ 01-fundamentos/4-introducao-a-javascript/4-javascript-objetos-e-funcoes/exercicios bonus/scripts.js
@@ -67,61 +67,116 @@
 
 // EXERCÍCIO BÔNUS 3
 
-const basket = [
-    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
-    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
-    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
-    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
-    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
-    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
-    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
-    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
-    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
-    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
-    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
-    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
-    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
-    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
-    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
-    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
-    'Banana', 'Pera', 'Abacate', 'Uva',
-];
+// const basket = [
+//     'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+//     'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+//     'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+//     'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+//     'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+//     'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+//     'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+//     'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+//     'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+//     'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+//     'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+//     'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+//     'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+//     'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+//     'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+//     'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+//     'Banana', 'Pera', 'Abacate', 'Uva',
+// ];
 
-const separeteSingleWords = wordsArray => {
+// const separeteSingleWords = wordsArray => {
 
-    wordsArray.sort();
+//     wordsArray.sort();
 
-    const uniqueWords = [wordsArray[0]];
-    for (let index = 1; index < wordsArray.length; index += 1) {
-        if (wordsArray[index] !== wordsArray[index + 1] && index + 1 !== wordsArray.length) {
-            uniqueWords.push(wordsArray[index + 1])
-        }
-    }
-    return uniqueWords
-}
+//     const uniqueWords = [wordsArray[0]];
+//     for (let index = 1; index < wordsArray.length; index += 1) {
+//         if (wordsArray[index] !== wordsArray[index + 1] && index + 1 !== wordsArray.length) {
+//             uniqueWords.push(wordsArray[index + 1])
+//         }
+//     }
+//     return uniqueWords
+// }
 
-const joinRepetitionsInObject = (singleWords, wordsArray) => {
+// const joinRepetitionsInObject = (wordsArray) => {
+//     const singleWordsArray = separeteSingleWords(basket);
 
-    const singleWordsRepetitions = {};
-    const numberOfRepetitions = [];
+//     const singleWordsRepetitions = {};
+//     const numberOfRepetitions = [];
 
-    for (let value of singleWords) {
-        let count = 0;
-        for (let index in wordsArray) {
-            if (value === wordsArray[index]) {
-                count += 1
-            }
-        }
-        numberOfRepetitions.push(count);
-        for (let indexNumber in numberOfRepetitions) {
-            singleWordsRepetitions[value] = numberOfRepetitions[indexNumber];
-        }
-    }
+//     for (let value of singleWordsArray) {
+//         let count = 0;
+//         for (let index in wordsArray) {
+//             if (value === wordsArray[index]) {
+//                 count += 1
+//             }
+//         }
+//         numberOfRepetitions.push(count);
+//         for (let indexNumber in numberOfRepetitions) {
+//             singleWordsRepetitions[value] = numberOfRepetitions[indexNumber];
+//         }
+//     }
 
-    return singleWordsRepetitions
-}
+//     return singleWordsRepetitions
+//   }
 
-console.log(joinRepetitionsInObject(separeteSingleWords(basket), basket));                  // EXERCÍCIO BÔNUS 3
+//   console.log(joinRepetitionsInObject(basket));                 // EXERCÍCIO BÔNUS 3
 
 
 // EXERCÍCIO BÔNUS 4
+
+let moradores = {
+    blocoUm: [
+        {
+            nome: 'Luiza',
+            sobrenome: 'Guimarães',
+            andar: 10,
+            apartamento: 1005,
+        },
+        {
+            nome: 'William',
+            sobrenome: 'Albuquerque',
+            andar: 5,
+            apartamento: 502,
+        },
+    ],
+    blocoDois: [
+        {
+            nome: 'Murilo',
+            sobrenome: 'Ferraz',
+            andar: 8,
+            apartamento: 804,
+        },
+        {
+            nome: 'Zoey',
+            sobrenome: 'Brooks',
+            andar: 1,
+            apartamento: 101,
+        },
+    ],
+};
+
+// console.log(`O morador do bloco 2 de nome ${moradores.blocoDois[1]['nome']} ${moradores.blocoDois[1]['sobrenome']} mora no ${moradores.blocoDois[1]['andar']}° andar, apartamento ${moradores.blocoDois[1]['apartamento']} `);           // Requisito 1
+
+const showFullName = (bloco) => {
+
+    const pessoas = [];
+
+    for (let index in bloco) {
+        let fullName = '';
+        for (let key in bloco[index]) {
+            if (key === 'nome') {
+                fullName += `${bloco[index][key]}`;
+            } else if (key === 'sobrenome') {
+                fullName += ` ${bloco[index][key]}`;
+            }
+        }
+        pessoas.push(fullName);
+    }
+    return pessoas.join(', ');
+}
+
+console.log(showFullName(moradores.blocoUm));
+console.log(showFullName(moradores.blocoDois));        // Requisito 2 
