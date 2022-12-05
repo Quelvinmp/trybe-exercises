8bbffdcec5a658e02d2d4160ca300cfe97905e09
Quelvinmp <quelvinmp@gmail.com>
2022-12-05 10:29:28 -0300

    resolve fixando aprendizado - seletores de elementos

---
+++ 01-fundamentos/5-javascript-dom-eventos-webstorage/1-javascript-dom-e-seletores/scripts.js
@@ -0,0 +1,13 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Professor Peso Pesado';
+document.getElementById('second-paragraph').innerText = 'O melhor filme de todos os tempos!';
+document.getElementById('subtitle').innerText = 'Característica Principal';
+
+const paragrafo = document.getElementsByClassName('paragraphs')[0];
+paragrafo.style.color = 'blue';
+
+const subtitle = document.getElementsByTagName('h4')[0];
+subtitle.style.color = 'green';
+
