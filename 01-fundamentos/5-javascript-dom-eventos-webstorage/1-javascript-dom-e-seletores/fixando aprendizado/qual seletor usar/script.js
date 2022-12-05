ac9e55455fd92b0c49e4ff0f34f6decba22bdce7
Quelvinmp <quelvinmp@gmail.com>
2022-12-05 11:09:33 -0300

    resolve fixando aprendizado

---
+++ 01-fundamentos/5-javascript-dom-eventos-webstorage/1-javascript-dom-e-seletores/fixando aprendizado/qual seletor usar/script.js
@@ -0,0 +1,15 @@
+const emergencyTasks = document.getElementsByTagName('section');
+emergencyTasks[0].style.backgroundColor = 'rgb(254, 159, 132)';
+emergencyTasks[1].style.backgroundColor = 'rgb(249, 219, 94)';
+
+const noEmergencyTasks = document.getElementsByTagName('h3');
+noEmergencyTasks[0].style.backgroundColor = 'rgb(165, 0, 243)';
+noEmergencyTasks[1].style.backgroundColor = 'rgb(165, 0, 243)';
+noEmergencyTasks[2].style.backgroundColor = 'rgb(36, 37, 37)';
+noEmergencyTasks[3].style.backgroundColor = 'rgb(36, 37, 37)';
+
+const title = document.getElementById('header-container');
+title.style.backgroundColor = 'rgb(0, 176, 105';
+
+const footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'rgb(0, 53, 51';
