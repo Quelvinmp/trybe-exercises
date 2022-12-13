316edba4924bcb6c1bb264d0faf27bc1cb3ff98c
Quelvinmp <quelvinmp@gmail.com>
2022-12-13 15:03:54 -0300

    atualiza

--- 01-fundamentos/5-javascript-dom-eventos-webstorage/3-javascript-eventos/exercicio/script.js
+++ 01-fundamentos/5-javascript-dom-eventos-webstorage/3-javascript-eventos/exercicio/script.js
@@ -16,7 +16,6 @@ createDaysOfTheWeek();
 // Escreva seu código abaixo.
 
 const createDaysOfTheMonth = () => {
-    const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     const ulDays = document.getElementById('days');
 
     for (const day of decemberDaysList) {
@@ -31,6 +30,7 @@ const createDaysOfTheMonth = () => {
         ulDays.appendChild(dayElement);
     }
 }
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 createDaysOfTheMonth()
 
 const createButtonHolidays = (buttonName) => {
