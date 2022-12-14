2f69320476562b55e741a2a63b8ef5c6b4dcc695
Quelvinmp <quelvinmp@gmail.com>
2022-12-14 11:57:49 -0300

    implementa estrutura do site

---
+++ 01-fundamentos/5-javascript-dom-eventos-webstorage/4-javascript-webstorage/exercicio/script.js
@@ -0,0 +1,79 @@
+const backgroundColor = document.getElementById('bg-container');
+const colorsBgc = ['White', 'Black', 'Green', 'Blue', 'Yellow'];
+
+const fontColor = document.getElementById('fontcolor-container');
+const colorsFont = ['Black', 'Red', 'White'];
+
+const fontSize = document.getElementById('fontsize-container');
+const optionsSize = ['8pt', '10pt', '12pt', '14pt', '20pt'];
+
+const spaceBetweenLines = document.getElementById('spacebtwl-container');
+const optionsSpace = ['1', 'Normal', '1.5', '2.0', '3.0'];
+
+const fontType = document.getElementById('fonttype-container');
+const optionsTypes = ['Arial', 'Times New Roman'];
+
+// console.log(backgroundColor);
+const createBtnBgc = (options) => {
+    for (let index = 0; index < options.length; index += 1) {
+        const btnBgc = document.createElement('button');
+        btnBgc.innerHTML = options[index];
+        btnBgc.id = `bgc-${index}`;
+        btnBgc.classList.add('paternBtn');
+        
+        backgroundColor.appendChild(btnBgc);
+    }
+}
+
+const createBtnFontC = (options) => {
+    for (let index = 0; index < options.length; index += 1) {
+        const btnFontC = document.createElement('button');
+        btnFontC.innerHTML = options[index];
+        btnFontC.id = `fontc-${index}`;
+        btnFontC.classList.add('paternBtn');
+        fontColor.appendChild(btnFontC);
+    }
+}
+
+const createBtnFontS = (options) => {
+    for (let index = 0; index < options.length; index += 1) {
+        const btnFontS = document.createElement('button');
+        btnFontS.innerHTML = options[index];
+        btnFontS.id = `fonts-${index}`;
+        btnFontS.classList.add('paternBtn');
+        fontSize.appendChild(btnFontS);
+    }
+}
+
+const createBtnSpaceLines = (options) => {
+    for (let index = 0; index < options.length; index += 1) {
+        const btnSpaceL = document.createElement('button');
+        btnSpaceL.innerHTML = options[index];
+        btnSpaceL.id = `spacel-${index}`;
+        btnSpaceL.classList.add('paternBtn');
+        spaceBetweenLines.appendChild(btnSpaceL);
+    }
+}
+
+const createBtnFontTypes = (options) => {
+    for (let index = 0; index < options.length; index += 1) {
+        const btnFontT = document.createElement('button');
+        btnFontT.innerHTML = options[index];
+        btnFontT.id = `fontt-${index}`;
+        btnFontT.classList.add('paternBtn');
+        fontType.appendChild(btnFontT);
+    }
+}
+
+
+
+
+
+
+window.onload = () => {
+    createBtnBgc(colorsBgc);
+    createBtnFontC(colorsFont);
+    createBtnFontS(optionsSize);
+    createBtnSpaceLines(optionsSpace);
+    createBtnFontTypes(optionsTypes);
+}
