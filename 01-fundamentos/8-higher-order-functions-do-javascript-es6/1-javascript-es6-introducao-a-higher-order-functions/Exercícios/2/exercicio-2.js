da4615e4e72e3292d37cf8b45ec814b326c56711
Quelvinmp <quelvinmp@gmail.com>
2023-01-25 18:59:14 -0300

    exercicio concluido 2

--- 01-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/Exercícios/2/exercicio-2.js
+++ 01-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/Exercícios/2/exercicio-2.js
@@ -1,2 +1,18 @@
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+
+const corrigeRespostas = () => {
+    let count = 0;
+    RIGHT_ANSWERS.map((a, b) => {
+        console.log(a,STUDENT_ANSWERS[b]);
+        if (a === STUDENT_ANSWERS[b]) {
+            count += 1;
+        } else if (a !== STUDENT_ANSWERS[b] && STUDENT_ANSWERS[b] !== 'N.A') {
+            count -= 0.5;
+        }
+    })
+    return count
+};
+
+console.log(corrigeRespostas());
---
+++ 01-fundamentos/8-higher-order-functions-do-javascript-es6/1-javascript-es6-introducao-a-higher-order-functions/Exercícios/4/script.js
@@ -0,0 +1,7 @@
+const loteria = (number) => {
+    const sorteado = Math.ceil(Math.random() * 6);
+    // ver no course
+}
+
+console.log(loteria(2));
+
