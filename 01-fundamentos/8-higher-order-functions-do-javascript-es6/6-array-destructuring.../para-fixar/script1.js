b62e44eb21044fb036c6bcf29b0efc39540ddbf7
Quelvinmp <quelvinmp@gmail.com>
2023-01-31 14:56:55 -0300

    resolve exercicios

---
+++ 01-fundamentos/8-higher-order-functions-do-javascript-es6/6-array-destructuring.../para-fixar/script1.js
@@ -0,0 +1,36 @@
+// const primeNumbers = [17, 23, 37]
+
+// const [ um, dois, tres ] = primeNumbers;
+
+// const sum = (a, b) => {
+//   console.log(a + b);
+// }
+
+// sum(um, tres) // 54
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+// =====
+
+// let comida = 'gato';
+// let animal = 'água';
+// let bebida = 'arroz';
+
+// [comida, animal, bebida] = [bebida, comida, animal];
+
+
+// console.log(comida, animal, bebida); // arroz gato água
+
+// // // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+// =====
+
+// let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+// // console.log(numerosPares); // [6, 8, 10, 12];
+
+// // Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+
+// [,,, ...numerosPares] = numerosPares;
+
+// console.log(numerosPares); // [6, 8, 10, 12];
