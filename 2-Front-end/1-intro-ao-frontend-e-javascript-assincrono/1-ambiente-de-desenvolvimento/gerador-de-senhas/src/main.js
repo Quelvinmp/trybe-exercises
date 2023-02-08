f1ef5570877376b8626f4a438edbf9403749fb74
Quelvinmp <quelvinmp@gmail.com>
2023-02-08 09:57:36 -0300

    estiliza aplicação

--- 2-Front-end/1-intro-ao-frontend-e-javascript-assincrono/1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ 2-Front-end/1-intro-ao-frontend-e-javascript-assincrono/1-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,4 +1,5 @@
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid';
+import './style.css';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
@@ -7,4 +8,3 @@ passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
 });
-
