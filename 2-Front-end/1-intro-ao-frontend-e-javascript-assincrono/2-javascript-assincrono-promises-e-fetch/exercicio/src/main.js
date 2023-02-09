24159b6e99a0d4b3cc6f29c540f9e038198535c3
Quelvinmp <quelvinmp@gmail.com>
2023-02-09 16:05:20 -0300

    implementa exercicio

---
+++ 2-Front-end/1-intro-ao-frontend-e-javascript-assincrono/2-javascript-assincrono-promises-e-fetch/exercicio/src/main.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-magic-numbers */
+import Swal from 'sweetalert2';
+import './style.css';
+
+const randomBtn = document.getElementById('random');
+
+randomBtn.addEventListener('click', () => {
+  const randomId = Math.floor(Math.random() * 731) + 1;
+
+  fetch(`https://www.superheroapi.com/api.php/5619471344845487/${randomId}`)
+    .then((response) => response.json())
+    .then((data) => Swal.fire({
+      text: `${data.name}`,
+      imageUrl: `${data.image.url}`,
+      imageAlt: `${data.image.name}`,
+    }))
+    .catch((error) => Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: `${error.message}`,
+      footer: '<a href="">Why do I have this issue?</a>',
+    }));
+});
