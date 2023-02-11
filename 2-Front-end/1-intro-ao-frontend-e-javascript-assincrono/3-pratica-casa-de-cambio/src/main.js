b71bf5dbe0b0a8c024dfbdbfbcd0041194f5c5b8
Quelvinmp <quelvinmp@gmail.com>
2023-02-11 10:55:56 -0300

    implementa novas features: input valor, input de destino de conversão e swap button

--- 2-Front-end/1-intro-ao-frontend-e-javascript-assincrono/3-pratica-casa-de-cambio/src/main.js
+++ 2-Front-end/1-intro-ao-frontend-e-javascript-assincrono/3-pratica-casa-de-cambio/src/main.js
@@ -6,15 +6,33 @@ const searchBtn = document.getElementById('search');
 const chooseCoin = document.getElementById('choose-coin');
 const hText = document.getElementById('dinamic-text');
 const coinList = document.getElementById('dinamic-list');
+const inputConversion = document.getElementById('conversion');
+const inputValue = document.getElementById('value');
+const swap = document.getElementById('swap');
 
-const createDivs = (coin, value) => {
+swap.addEventListener('click', () => {
+  const actuallyValueConversion = inputConversion.value;
+  const actuallyValueChosenCoin = chooseCoin.value;
+  chooseCoin.value = actuallyValueConversion;
+  inputConversion.value = actuallyValueChosenCoin;
+});
+
+const readAPI = () => fetch(`https://api.exchangerate.host/latest?base=${chooseCoin.value}`)
+  .then((response) => response.json());
+
+const createDivs = (infos) => {
+  const [coin, value] = infos;
   const coinDiv = document.createElement('div');
   const coinLogo = document.createElement('img');
   coinLogo.src = '../assets/coin.svg';
   const coinP = document.createElement('p');
   const coinValue = document.createElement('p');
   coinP.innerHTML = coin;
-  coinValue.innerHTML = value.toFixed(3);
+  if ((chooseCoin.value.toUpperCase() === 'BTC' || inputConversion.value.toUpperCase() === 'BTC' || coin === 'BTC') && value < 1) {
+    coinValue.innerHTML = value.toFixed(8);
+  } else {
+    coinValue.innerHTML = value.toFixed(3);
+  }
   coinDiv.appendChild(coinLogo);
   coinDiv.appendChild(coinP);
   coinDiv.appendChild(coinValue);
@@ -30,32 +48,52 @@ const clearTable = () => {
 const coinVerify = (name) => Object.keys(name)
   .some((coin) => coin === chooseCoin.value.toUpperCase());
 
+const compareCurrencies = (object) => {
+  clearTable();
+  const conversion = Object.entries(object)
+    .find((rate) => rate[0] === inputConversion.value.toUpperCase());
+  hText.innerHTML = `Valores referentes a ${inputValue.value} ${chooseCoin.value.toUpperCase()} (1 und = ${conversion[1]} ${conversion[0]})`;
+  conversion[1] *= inputValue.value;
+  createDivs(conversion);
+};
+
 searchBtn.addEventListener('click', () => {
-  if (chooseCoin.value === '') {
+  if (!chooseCoin.value) {
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
-      text: 'Você precisa passar uma moeda',
+      text: 'Você precisa passar uma moeda de conversão!',
     });
-  } else {
-    fetch(`https://api.exchangerate.host/latest?base=${chooseCoin.value}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const { rates } = data;
-        if (coinVerify(rates)) {
-          clearTable();
-          hText.innerHTML = `Valores referentes a 1 ${chooseCoin.value.toUpperCase()}`;
-          Object.entries(rates).forEach((value) => {
-            createDivs(value[0], value[1]);
-          });
-        } else {
-          throw new Error('Moeda Não existente!');
-        }
-      })
-      .catch((error) => Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: error,
-      }));
+    return;
   }
+  readAPI()
+    .then((data) => {
+      const { rates } = data;
+      if (inputConversion.value) {
+        compareCurrencies(rates);
+        return;
+      }
+      if (coinVerify(rates)) {
+        clearTable();
+        hText.innerHTML = `Valores referentes a ${inputValue.value} ${chooseCoin.value.toUpperCase()}`;
+        Object.entries(rates).forEach((value) => {
+          createDivs(value);
+        });
+      } else {
+        throw new Error('Moeda Não existente!');
+      }
+    })
+    .catch((error) => Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: error,
+    }));
 });
+
+window.onload = () => {
+  readAPI()
+    .then((data) => {
+      const { rates } = data;
+      compareCurrencies(rates);
+    });
+};
