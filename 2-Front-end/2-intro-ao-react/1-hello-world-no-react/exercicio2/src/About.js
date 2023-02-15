46f8479b6ba06c1d3fe864656e70dc42eb908f93
Quelvinmp <quelvinmp@gmail.com>
2023-02-15 15:46:48 -0300

    realizar exercicio2

---
+++ 2-Front-end/2-intro-ao-react/1-hello-world-no-react/exercicio2/src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Quelvin</h1>
+        <p>22 anos, friburguense</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+        </ul>
+      </div>
+    )
+    
+  }
+}
+
+export default AboutMe
